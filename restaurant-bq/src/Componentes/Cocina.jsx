ff3dab5a62ce5fe01862fc9b42e4b795244ea195
keiry <keiryquero@gmail.com>
2021-11-02 10:30:09 -0500

    Creación de Router con React

---
+++ restaurant-bq/src/Componentes/Cocina.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Cocina = () =>{
+    return(
+        <h1>Vista de la Cocina</h1>
+    );
+}
+export { Cocina };
---
+++ restaurant-bq/src/Componentes/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Home = () =>{
+    return(
+        <h1>Vista del Home </h1>
+    );
+}
+export { Home };
