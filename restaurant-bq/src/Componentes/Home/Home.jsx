1dacfaabdc3adc2158d5079fac49aacc3118b0a9
keiry <keiryquero@gmail.com>
2021-11-04 11:50:55 -0500

    creacion de carpeta imagenes,inclusión del logo y fondo al Home

--- restaurant-bq/src/Componentes/Home/Home.jsx
+++ restaurant-bq/src/Componentes/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
+import './Home.css'
+import Logo from '../Imagenes/logo.png'
 
 const Home = () => {
   return (
     <Fragment>
       <div className="container-home">
-        <img>
-        
-        </img>
+        <img src={Logo}/>
         <div>
           <a href="/mesero">MESERO</a>
           <a href="/cocina">JEFE DE COCINA</a>
