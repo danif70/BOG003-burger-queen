764562934c0810c0153b6a56558d1c6544c2c2ad
Daniela Funes <danielafunes1970@gmail.com>
2021-11-09 12:05:07 -0500

    se crea data2.json, se trae data json de desayuno a tarjetas de componente breakfastmenu, se agregó imagen de cafe(no se ve aun), se hizo funcionalidad de renderización del componente breakfastmenu cuando se activa boton desayuno

--- restaurant-bq/src/Componentes/BotonesMenu/Botonesmenu.jsx
+++ restaurant-bq/src/Componentes/BotonesMenu/Botonesmenu.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import  { Fragment } from "react";
+//import { BreakfastMenu } from '../Menus/MenuDesayuno';
 
-const BtnMenu = () => {
+//la función recibe el props(prueba) y se le pasa el evento del botón desayuno
+const BtnMenu = ({prueba}) => {
+  
+
+
   return (
     <Fragment>
       <div className="container-btn-menu">
-        <button className="btn-menu btn btn-outline-warning">DESAYUNOS</button>
+        <button onClick={prueba} className="btn-menu btn btn-outline-warning">DESAYUNOS</button>
         <button className="btn-menu btn btn-outline-warning">COMIDAS</button>
       </div>
     </Fragment>
--- restaurant-bq/src/Componentes/Mesero/Mesero.jsx
+++ restaurant-bq/src/Componentes/Mesero/Mesero.jsx
@@ -1,16 +1,25 @@
-import React, { Fragment } from "react";
+import React, { useState,Fragment } from "react";
 import { Header } from '../Header/Header';
 import { BtnMenu } from "../BotonesMenu/Botonesmenu";
 import { BreakfastMenu } from "../Menus/MenuDesayuno";
 import "./Mesero.css"
 
 const Mesero = () => {
+    //creamos un props(prueba) que maneja el estado del boton desayuno que inicializa en false, se le pasa al componente <BtnMenu> 
+    const [bfMenu, setBfMenu]= useState(false)
+    const prueba = () =>{
+        setBfMenu(true)
+        console.log('prueba botones', bfMenu)
+    }
+    
     return (
         <Fragment>
             <div className="container-waiter vh-100">
                 <Header />
-                <BtnMenu />
-                <BreakfastMenu />
+                <BtnMenu prueba={prueba} />
+                {/*aquí se maneja el cambio de estado con un operador ternario que pregunta si la variable de estado(bfMenu) es true se llama al componente BreakfastMenu*/}
+                {bfMenu?<BreakfastMenu />:''}
+                 
             </div>
         </Fragment>
     );
