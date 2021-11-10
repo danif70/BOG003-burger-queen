8aa948a875b448b09526a8b52d2c6cdfa01a5212
keiry <keiryquero@gmail.com>
2021-11-10 13:47:04 -0500

    manipulacion del estado de los bototones en seccion Botonesmenu, se creó componente MenuAlmuerzo para filtrar las opciones de comidas

--- restaurant-bq/src/Componentes/BotonesMenu/Botonesmenu.jsx
+++ restaurant-bq/src/Componentes/BotonesMenu/Botonesmenu.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import  { Fragment } from "react";
-//import { BreakfastMenu } from '../Menus/MenuDesayuno';
+import { BreakfastMenu } from "../Menus/MenuDesayuno";
+import { LunchMenu } from "../Menus/MenuAlmuerzo";
 
-//la función recibe el props(prueba) y se le pasa el evento del botón desayuno
-const BtnMenu = ({prueba}) => {
-  
-
-
+const BtnMenu = () => {
+  const [bfMenu, setBfMenu] = useState("DESAYUNOS");
+  /* const [lunchMenu, setLunchMenu] = useState(false);
+   */
   return (
     <Fragment>
       <div className="container-btn-menu">
-        <button onClick={prueba} className="btn-menu btn btn-outline-warning">DESAYUNOS</button>
-        <button className="btn-menu btn btn-outline-warning">COMIDAS</button>
+        <button onClick={()=>{setBfMenu("DESAYUNOS")}} className="btn-menu btn btn-outline-warning">DESAYUNOS</button>
+       {/*  {bfMenu?<BreakfastMenu />:""}  */}
+        <button onClick={()=>{setBfMenu("COMIDAS")}} className="btn-menu btn btn-outline-warning">COMIDAS</button>
+        {/* {lunchMenu?<LunchMenu />:""}  */}
+      {bfMenu === "DESAYUNOS"?<BreakfastMenu />: <LunchMenu />}
+
       </div>
     </Fragment>
   )
